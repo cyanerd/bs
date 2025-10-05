f1d3e392bfa721b27710fcba9d0e85175ade132d
alex <alexeleave@gmail.com>
2025-10-05 21:06:01 +0300

    fix: wallet logout doesn not touch twitter auth

--- src/hooks/useUserAuth.ts
+++ src/hooks/useUserAuth.ts
@@ -41,8 +41,6 @@ export function useUserAuth() {
   const logout = () => {
     setWalletAddress("");
     setWalletConnected(false);
-    setTwitterConnected(false);
-    setName("");
   };
 
   useEffect(() => {

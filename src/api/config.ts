3976429f2ab0865156129f339cac3e4fa506c612
Your Name <you@example.com>
2025-10-19 14:30:37 +0200

    urls

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
-export const API_HOST = "https://raffle.blockstranding.com/api";
-export const API_X_CALLBACK = `${API_HOST}/raffle/auth/twitter`;
+export const API_HOST = "https://presale.blockstranding.com/api";
+export const API_X_CALLBACK = `${API_HOST}/presale/auth/twitter`;
